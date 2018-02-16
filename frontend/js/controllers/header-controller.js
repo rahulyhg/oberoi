1e9294771a90d2fd76cd72a029e1546b0a784bf9
Srujana <srujana.vemula@wohlig.com>
2018-02-16 18:53:45 +0530

    responsive done in ipad and phone

--- frontend/js/controllers/header-controller.js
+++ frontend/js/controllers/header-controller.js
@@ -1,7 +1,16 @@
-myApp.controller('headerCtrl', function ($scope, TemplateService) {
+myApp.controller('headerCtrl', function ($scope, TemplateService, NavigationService) {
     $scope.template = TemplateService;
     $scope.$on('$stateChangeSuccess', function (event, toState, toParams, fromState, fromParams) {
         $(window).scrollTop(0);
     });
     $.fancybox.close(true);
+
+     $scope.nav=NavigationService.getNavigation();
+    console.log("Nav",$scope.nav);
+    // console.log("headerMenu",$scope.headerMenu);
+    $scope.changeView = function () {
+        console.log("changeTo",changeTo);
+        // $scope.check = NavigationService.getNavigation();
+        // console.log("$scope.navigation",$scope.check);
+    };
 });
